0b061fbf2ea7997109ed0f29cbfbbecbcd4f6e2a
Rajesh Joshi <rajeshjoshi607@gmail.com>
2021-01-03 22:37:30 +0530

    Made in 7 mins

---
+++ randomNum11to20.js
@@ -0,0 +1,5 @@
+Math.floor(Math.random()*10+10) 
+//Step1: Math.random() inbuild javascript function which returns value from 0 to 0.99999
+//Step2: Math.random()*10 gives us value from 0 to 9.9999999999
+//Step3: Math.random()*10+10 gives us value from 11 to 20.99999999999999
+//Math.floor() removed the decimals from result of step 3, and gives us value from 11 to 20
